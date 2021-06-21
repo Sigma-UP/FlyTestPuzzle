6dc32181211b2f56345dda8f29ed68234773a4c6
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-06-21 11:38:00 +0300

    add console Interface

---
+++ src/FlyTestPuzzle.BusinessLogic/FlyConsole/IConsole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlyTestPuzzle.src.FlyTestPuzzle.BusinessLogic.FlyConsole
+{
+    public interface IConsole
+    {
+            public void WriteLine(string text);
+    }
+}
